d4b13eb66fc273cb03cf611ff13e98342846a857
OlszewskiJacob <jakubolszewski1996@outlook.com>
2023-01-13 13:13:06 +0100

    js functionality basics, minor changes with css: portfolio section

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//slider: portfolio
 var slideIndex = 1;
 showDivs(slideIndex);
 
@@ -8,15 +9,54 @@ function plusDivs(n) {
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("Slider-Portfolio");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
+  if (n > x.length) { slideIndex = 1 }
+  if (n < 1) { slideIndex = x.length }
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+    x[i].style.display = 'none';
   }
-  x[slideIndex-1].style.display = "block";  
+  x[slideIndex - 1].style.display = 'block';
+}
+//scroll navbar
+function scrollHomeDiv() {
+  window.scroll(0,
+    findPosition(document.getElementById("Home-Call")));
+}
+function scrollSkillsDiv() {
+  window.scroll(0,
+    findPosition(document.getElementById("Skills-Call")));
+}
+function scrollAboutMeDiv() {
+  window.scroll(0,
+    findPosition(document.getElementById("About-me-Call")));
+}
+function scrollPortfolioDiv() {
+  window.scroll(0,
+    findPosition(document.getElementById("Portfolio-Call")));
+}
+function scrollContactDiv() {
+  window.scroll(0,
+    findPosition(document.getElementById("Contact-Call")));
+}
+function scrollContactDiv() {
+  window.scroll(0,
+    findPosition(document.getElementById("Lets-talk-Call")));
 }
 
-
-// scroll onclick
-// ar objDiv = document.getElementById("your_div");
-// objDiv.scrollTop = objDiv.scrollHeight;
+function findPosition(obj) {
+  var currenttop = 0;
+  if (obj.offsetParent) {
+    do {
+      currenttop += obj.offsetTop;
+    } while ((obj = obj.offsetParent));
+    return [currenttop];
+  }
+}
+//contact mail
+function sendMail() {
+  var link = "mailto:jakubolszewski1996@outlook.com"
+          //  + "&subject=" + encodeURIComponent("Portfolio:")
+          //  + "&body=" + encodeURIComponent(document.getElementById('myText').value)
+  ;
+  
+  window.location.href = link;
+}
