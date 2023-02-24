ece077864e34b012e6c9e90affac1e2a982c40f5
Jakub Olszewski <jakubolszewski1996@outlook.com>
2023-02-24 12:28:25 +0100

    Portfolio Changes in layout

--- js/script.js
+++ js/script.js
@@ -125,24 +125,27 @@ function reveal() {
 }
 
 
-//slider: portfolio
-let slideIndex = 1;
-showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
+// slider: portfolio
+// let slideIndex = 1;
+// showDivs(slideIndex);
+// function plusDivs(n) {
+// showDivs(slideIndex += n);
+// }
+// function showDivs(n) {
+// let i;
+// let x = document.getElementsByClassName("Slider-Portfolio");
+// if (n > x.length) { slideIndex = 1 }
+// if (n < 1) { slideIndex = x.length }
+// for (i = 0; i < x.length; i++) {
+// x[i].style.display = 'none';
+// }
+// x[slideIndex - 1].style.display = 'block';
+// }
 
-function showDivs(n) {
-  let i;
-  let x = document.getElementsByClassName("Slider-Portfolio");
-  if (n > x.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = x.length }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = 'none';
-  }
-  x[slideIndex - 1].style.display = 'block';
-}
+
+
+
 
 
 //scroll navbar
